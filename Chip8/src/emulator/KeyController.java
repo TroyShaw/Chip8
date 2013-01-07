4821ed85f12536eee26fbf856e249fca06b8827b
Troy Shaw <troyshw@gmail.com>
2013-01-07 17:40:37 +1300

    Added documentation.

--- Chip8/src/emulator/KeyController.java
+++ Chip8/src/emulator/KeyController.java
@@ -1,12 +1,16 @@
 package emulator;
 
+import emulator.exception.InvalidKeyException;
+
 public interface KeyController {
 
 	/**
-	 * Signals that the given button was either pressed or released.
+	 * Signals that the given key was either pressed or released.<br>
+	 * The key must be between 0 and 15 inclusive.
 	 * 
-	 * @param b the button
+	 * @param k the key
 	 * @param pressed true if pressed, false if released
+	 * @throws InvalidKeyException if not in range 0-15 inclusive
 	 */
-	public void keyInteracted(int b, boolean pressed);
+	public void keyInteracted(int k, boolean pressed) throws InvalidKeyException;
 }
